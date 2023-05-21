a5b6a9cb00a386b4b95ee441575e10be6bb8ef37
joao-soubihe-sinch <joao.soubihe@sinch.com>
2023-05-20 21:19:40 -0300

    Initial structure for Java maven project

---
+++ src/main/java/com/example/redisStudy/RedisStudy.java
@@ -0,0 +1,13 @@
+package com.example.redisStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisStudy {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisStudy.class, args);
+    }
+
+}
