73dea74074ff0f730ec4941e8aafd2ceffdf1d9c
joao-soubihe-sinch <joao.soubihe@sinch.com>
2023-06-14 20:13:30 -0300

    Add integration tests

---
+++ src/test/java/com/example/redisStudy/integrationTest/AccountIntegrationTest.java
@@ -0,0 +1,174 @@
+package com.example.redisStudy.integrationTest;
+
+import com.example.redisStudy.controllers.AccountController;
+import com.example.redisStudy.model.Account;
+import com.example.redisStudy.service.AccountService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.UUID;
+
+import static com.example.redisStudy.TestConstants.BASE_ACCOUNT_URL_SUFFIX;
+import static com.example.redisStudy.TestConstants.BASE_URL;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+public class AccountIntegrationTest {
+
+    @Autowired
+    RedisTemplate<String, Account> redisAccountTemplate;
+
+    @Autowired
+    AccountService accountService;
+
+    @Autowired
+    AccountController accountController;
+
+    private final Account testAccount = Account.builder()
+            .accountId("123")
+            .accountName("testAccount")
+            .build();
+
+    private Account toUpdateAccount;
+
+    @Test
+    public void saveRetrieveAndDeleteAccountSuccessfully() {
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .post()
+                .uri(BASE_ACCOUNT_URL_SUFFIX)
+                .bodyValue(testAccount)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo(testAccount.getAccountId());
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .get()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX,testAccount.getAccountId()))
+                .exchange()
+                .expectBody(Account.class)
+                .isEqualTo(testAccount);
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .delete()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX,testAccount.getAccountId()))
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void deleteNonExistentAccountShouldReturn2xx() {
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .delete()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, UUID.randomUUID().toString()))
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+    }
+
+    @Test
+    public void getNonExistentAccountShouldReturn404NotFoundStatus() {
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .get()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, UUID.randomUUID().toString()))
+                .exchange()
+                .expectStatus()
+                .isNotFound();
+    }
+
+    @Test
+    public void shouldUpdateExistentAccountSuccessfully() {
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .post()
+                .uri(BASE_ACCOUNT_URL_SUFFIX)
+                .bodyValue(testAccount)
+                .exchange()
+                .expectBody(String.class)
+                .isEqualTo(testAccount.getAccountId());
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .get()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, testAccount.getAccountId()))
+                .exchange()
+                .expectBody(Account.class)
+                .isEqualTo(testAccount);
+
+        toUpdateAccount = Account.builder()
+                .accountId("123")
+                .accountName("NewName")
+                .build();
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .put()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, testAccount.getAccountId()))
+                .bodyValue(toUpdateAccount)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .get()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, testAccount.getAccountId()))
+                .exchange()
+                .expectBody(Account.class)
+                .isEqualTo(toUpdateAccount);
+    }
+
+    @Test
+    public void shouldCreateNewAccountWhenUpdateNonExistentAccount() {
+        toUpdateAccount = Account.builder()
+                .accountId("123")
+                .accountName("NewName")
+                .build();
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .put()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, testAccount.getAccountId()))
+                .bodyValue(toUpdateAccount)
+                .exchange()
+                .expectStatus()
+                .is2xxSuccessful();
+
+        WebTestClient
+                .bindToServer()
+                .baseUrl(BASE_URL)
+                .build()
+                .get()
+                .uri(String.join("/", BASE_ACCOUNT_URL_SUFFIX, testAccount.getAccountId()))
+                .exchange()
+                .expectBody(Account.class)
+                .isEqualTo(toUpdateAccount);
+    }
+}
---
+++ src/test/java/com/example/redisStudy/unitTest/service/AccountServiceTest.java
@@ -0,0 +1,128 @@
+package com.example.redisStudy.unitTest.service;
+
+import com.example.redisStudy.exceptions.AccountNotFoundException;
+import com.example.redisStudy.model.Account;
+import com.example.redisStudy.service.AccountService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+
+import javax.management.AttributeNotFoundException;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class AccountServiceTest {
+
+    @InjectMocks
+    AccountService accountService;
+
+    @Mock
+    RedisTemplate<String, Account> accountRedisTemplate;
+
+    @Mock
+    private ValueOperations<String, Account> valueOperations;
+
+    private String accountId;
+
+    @BeforeEach
+    public void setup() {
+        accountId = UUID.randomUUID().toString();
+    }
+
+    @Test
+    public void shouldSuccessfullyCreateAccount() {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        Account account = createStandardTestAccount(accountId);
+
+        doNothing().when(valueOperations).set(anyString(), any(Account.class), anyLong(), any());
+
+        String createdAccountId = accountService.createAccount(account);
+        Assertions.assertEquals(accountId, createdAccountId);
+    }
+
+    @Test
+    public void shouldSuccessfullyCreateAccountWithNewId() {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        Account account = createStandardTestAccount(null);
+
+        doNothing().when(valueOperations).set(anyString(), any(Account.class), anyLong(), any());
+
+        String createdAccountId = accountService.createAccount(account);
+        Assertions.assertNotNull(createdAccountId);
+    }
+
+    @Test
+    public void shouldSuccessfullyGetAccount() throws AccountNotFoundException {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        Account account = createStandardTestAccount(accountId);
+
+        when(valueOperations.get("accounts:" + accountId))
+                .thenReturn(account);
+
+        Account retrievedAccount = accountService.getAccount(accountId);
+        Assertions.assertEquals(account, retrievedAccount);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfElementNotFoundWhenGetAccount() {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+
+        when(valueOperations.get("accounts:" + accountId))
+                .thenReturn(null);
+
+        Assertions.assertThrows(AccountNotFoundException.class, () -> accountService.getAccount(accountId));
+    }
+
+    @Test
+    public void shouldReturnTrueIfAccountSuccessfullyUpdated() {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        Account account = createStandardTestAccount(accountId);
+
+        doNothing().when(valueOperations).set(anyString(), any(Account.class), anyLong(), any());
+
+        Assertions.assertTrue(accountService.updateAccount(accountId, account));
+    }
+
+    @Test
+    public void shouldReturnFalseIfAccountUpdateFailed() {
+        when(accountRedisTemplate.opsForValue()).thenReturn(valueOperations);
+        Account account = createStandardTestAccount(accountId);
+
+        doThrow(RuntimeException.class).when(valueOperations)
+                .set(anyString(), any(Account.class), anyLong(), any());
+
+        Assertions.assertFalse(accountService.updateAccount(accountId, account));
+    }
+
+    @Test
+    public void shouldReturnTrueIfSuccessfullyDeleteAccount() {
+        when(accountRedisTemplate.delete("accounts:" + accountId)).thenReturn(true);
+        Assertions.assertTrue(accountService.deleteAccount(accountId));
+    }
+
+    @Test
+    public void shouldReturnFalseIfDeleteAccountFailed() {
+        when(accountRedisTemplate.delete("accounts:" + accountId)).thenThrow(RuntimeException.class);
+        Assertions.assertFalse(accountService.deleteAccount(accountId));
+    }
+
+    private Account createStandardTestAccount(String createdAccountId) {
+        return Account.builder()
+                .accountId(createdAccountId)
+                .accountName("TestAccount")
+                .build();
+    }
+}
